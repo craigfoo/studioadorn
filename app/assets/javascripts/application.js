ff6f2b49a54b308169bff73f541c1d3af8794bd1
craig.rettew <craig@zeptoindustries.com>
2015-01-16 18:02:20 -0500

    added jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require canvas.slider.fade
 //= require plugins
-
+//= require turbolinks
