00630f574250367e9507f505dfbc7477ba31a07f
craig.rettew <craig@zeptoindustries.com>
2015-01-21 16:09:31 -0500

    improved home page and added about and booking pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    get 'booking', :to => "booking#index"
+
+   get 'about', :to => "about#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
